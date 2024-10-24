bfcb0d2a1def44cba44a9d685ad7ffa1ac5c9d59
KarlAndrei61 <karlandrei61@gmail.com>
2024-10-24 10:47:26 +0200

    merge

--- resources/views/article/searched.blade.php
+++ resources/views/article/searched.blade.php
@@ -4,29 +4,24 @@
             <h1 class="mt-4 mb-1 titleindex">Risultati per la ricerca "{{ $query }}"</h1>
         </div>
         <div class="container">
-            <div class="row">
+            <div class="row d-flex">
                 @forelse($articles as $article)
                 <x-card :article="$article" />
                 @empty
-                <div class="container">
-                    <div class="row">
                         <div class="col-12">
-
-                            <div class="container card cardBag">
-                                <h2 class="text-center mt-5 fs-3 revisorh2">Nessun articolo</h2>
+                            <div class="circle">
+                                <h2 class="text-center mt-5 fs-3 loremstyle">Nessun articolo</h2>
 
                                 <div class="d-flex justify-content-center mt-5">
                                     <img width="200" src="https://cdn3d.iconscout.com/3d/premium/thumb/shopping-bag-3d-icon-download-in-png-blend-fbx-gltf-file-formats--ecommerce-shop-user-interface-pack-icons-8988733.png?f=webp" alt="">
                                 </div>
 
-                                <a class="text-center linkstyle" href="{{route('article.create')}}">Crea Articolo</a>
+                                <a class="text-center loremstyle" href="{{route('article.create')}}">Crea Articolo</a>
 
 
                             </div>
 
                         </div>
-                    </div>
-                </div>
                 @endforelse
                 <div>
                     {{ $articles->links() }}
